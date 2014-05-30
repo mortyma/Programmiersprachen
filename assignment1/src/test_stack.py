2ca33832c0a05c5101e993016db921b3c6574ed1
Martin Kalany <m.kalany@gmx.at>
2014-05-30 20:45:10 +0200

    a simple unit test for stack

---
+++ assignment1/src/test_stack.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import unittest
+import stack
+
+class TestStack(unittest.TestCase):
+  
+  def setUp(self):
+    print "setUp"
+    self.stack = stack.Stack()
+
+  def test_basicStack(self):
+    print "bar"
+    self.stack.push(1)
+    self.stack.push(2)
+    self.assertEqual(2, self.stack.pop())
+    self.assertEqual(1, self.stack.pop())
+    
+if __name__ == '__main__':
+  unittest.main()
