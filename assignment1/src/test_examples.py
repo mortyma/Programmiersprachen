55756d1ced48ebd8948054f0e2dbec1d78a280ff
Martin Kalany <m.kalany@gmx.at>
2014-06-25 14:12:01 +0200

    test: 3 factorial

--- assignment1/src/test_examples.py
+++ assignment1/src/test_examples.py
@@ -23,5 +23,10 @@ class TestExamples(unittest.TestCase):
     self.calculator.execute()
     self.assertEqual([-9, 9], self.calculator.data)
      
+  def test_3_factorial(self):
+    code = "[2c1 3c-1c1=3c[][3c4d1+da]a2d*]2c3d2ca2d"
+    self.calculator.push_code("3"+ code)
+    self.calculator.execute()
+    self.assertEqual([6], self.calculator.data)
 if __name__ == "__main__":
   unittest.main()
