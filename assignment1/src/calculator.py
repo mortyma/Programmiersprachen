8707a0c540f1ccc686c80c61e37f977a5768ce70
Martin Kalany <m.kalany@gmx.at>
2014-06-26 13:01:14 +0200

    extract methods for commands

--- assignment1/src/calculator.py
+++ assignment1/src/calculator.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import operator
+import sys
 
 from parser import *
 import stack
@@ -30,7 +31,8 @@ class Calculator():
         #unary operators
         self.unaryOps = ["~"]
         # set of binary operators 
-        self.binaryOps = {"+": operator.add,
+        self.binaryOps = {
+            "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
             "/": self.div,
@@ -42,9 +44,18 @@ class Calculator():
             "=": self.eq
             }
         # commands
-        self.commands = ["a", "c", "d", "r", "w", "b", "g", "x"]
+        self.commands = {
+            "a": self.applyBlock,
+            "c": self.copy, 
+            "d": self.delete, 
+            "r": self.read, 
+            "w": self.write, 
+            "b": self.build, 
+            "g": self.group, 
+            "x": self.exit
+            }
         # union of commands, unaryOps and binaryOps
-        self.operations = self.unaryOps + self.binaryOps.keys() + self.commands
+        self.operations = self.unaryOps + self.binaryOps.keys() + self.commands.keys()
     
     def push_code(self, s):
         '''push code onto the calculator's code stack.
@@ -88,108 +99,121 @@ class Calculator():
         while (not self.code.isEmpty()):
             token = self.code.pop()
             if util.is_number(token):
-                    # integers are simply pushed onto the data stack
-                    self.data.push(int(token))
-            elif token == "x":
-                    # exit
-                    return
+                # integers are simply pushed onto the data stack
+                self.data.push(int(token))
             elif token in self.binaryOps.keys():
-                    # binary operators: {+,-,*,/,%,&,|,=,<,>}
-                    # pop to items from data stack 
-                    a = self.data.pop()
-                    b = self.data.pop()
-                    # get the operator
-                    op_func = self.binaryOps[token]
-                    if(token != "="):  # "=" does not require a and b to be integers
-                        #check that they are integers
-                        util.expectInt(a)
-                        util.expectInt(b)
-                    # apply the operator...
-                    c = int(op_func(a, b)) # some operators (i.e., "<" and ">" return "True" or "False"; so parse it to int
-                    # and push the result onto the data stack
-                    self.data.push(c)
+                # binary operators: {+,-,*,/,%,&,|,=,<,>}
+                # pop to items from data stack 
+                a = self.data.pop()
+                b = self.data.pop()
+                # get the operator
+                op_func = self.binaryOps[token]
+                if(token != "="):  # "=" does not require a and b to be integers
+                    #check that they are integers
+                    util.expectInt(a)
+                    util.expectInt(b)
+                # apply the operator...
+                c = int(op_func(a, b)) # some operators (i.e., "<" and ">" return "True" or "False"; so parse it to int
+                # and push the result onto the data stack
+                self.data.push(c)
             elif token == "~":
-                    # negation
-                    val = int(self.data.pop())
-                    self.data.push(-val)  
-            elif token == "c":
-                    # copy operator
-                    idx = self.data.peek() #can't use pop to get parameter n, because copy might want to use the 1st element
-                    util.expectPosIntSmallerThan(idx, self.data.size())
-                    val = self.data[idx] #get the value at stack depth n
-                    self.data.pop() #pop parameter n
-                    self.data.push(val) #push the new value
-            elif token == "d":
-                    # delete operator
-                    idx = self.data.peek() #can't use pop to get parameter n, because delete might want to use the 1st element
-                    util.expectPosIntSmallerThan(idx, self.data.size())
-                    val = self.data[idx] #get the value at stack depth n
-                    if(idx != 0):
-                        self.data.remove(idx)
-                    self.data.pop() #pop parameter n
-            elif token == "r":
-                    # read from input stream 
-                    item = chr(self.iS.read())
-                    # only digits, commands and operators may be read 
-                    if util.is_number(item):
-                        self.data.push(int(item))
-                    elif item in self.operations:
-                        self.data.push(item)
-            elif token == "w":
-                    # make sure that the top element of the data stack is an integer or a command
-                    val = self.data.pop()
-                    if not util.is_number(val) and val not in self.operations:
-                        raise ValueError("Cannot write to output stream" + val)
-                    for byte in to_ascii(val):
-                        self.oS.write(byte)
+                # negation
+                val = int(self.data.pop())
+                self.data.push(-val)  
+            elif token == "x":
+                return #quick fix: exit gracefully
+            elif token in self.commands.keys():
+                func = self.commands[token]
+                func()
             elif token.startswith("[") and token.endswith("]"):
-                    # block
-                    self.data.push(token)
-            elif token == "a":
-                    # apply
-                    if self.is_block(self.data.peek()):
-                        p = self.data.pop()
-                        self.push_code(p[1:len(p) - 1])
-            elif token == "g":
-                    # group
-                    arg1 = self.data.pop() # the argument on top of the stack will be at the end of the block
-                    arg2 = self.data.pop()
-                    if self.is_block(arg1) and self.is_block(arg2):
-                        # two blocks
-                        block =  arg2[0:len(arg2) - 1]
-                        arg1 = arg1[1:len(arg1)]
-                        if util.is_number(arg2[len(arg1) - 1]) and util.is_number(arg1[0]):
-                                block += " "
-                        block += arg1
-                    elif util.is_number(arg1) and util.is_number(arg2):
-                        # two integers
-                        block = "[" + str(arg2) + " " + str(arg1) + "]"
-                    elif util.is_number(arg2) and self.is_block(arg1):
-                        # interger (arg2) and block (arg1)
-                        block = "[" + str(arg2)
-                        arg1 = arg1[1:len(arg1)]
-                        if util.is_number(arg1[0]):
-                                block += " "
-                        block += arg1
-                    elif self.is_block(arg2) and util.is_number(arg1):
-                        # block (arg2) and integer (arg1)
-                        block =  arg2[0:len(arg2) - 1]
-                        if util.is_number(arg2[len(arg2) - 1]) and util.is_number(arg1[0]):
-                                block += " "
-                        block += str(arg1) + "]"
-                    else:
-                        raise ValueError("todo")
-                    self.data.push(block)
-            elif token == "b":
-                    arg = self.data.pop()
-                    if self.is_block(arg) or arg in self.operations:
-                        self.data.push("[" + arg + "]")
-                    else:
-                        raise ValueError("Invalid argument for operation 'build block':" + str(arg))
+                self.data.push(token)
             else:
-                    msg = "Unknown command: " + token
-                    raise ValueError(msg)
+                msg = "Unknown command: " + token
+                raise ValueError(msg)
       
+    def exit(self):
+        sys.exit(0)
+      
+    def copy(self):
+        ''' operator "c" '''
+        idx = self.data.peek() #can't use pop to get parameter n, because copy might want to use the 1st element
+        util.expectPosIntSmallerThan(idx, self.data.size())
+        val = self.data[idx] #get the value at stack depth n
+        self.data.pop() #pop parameter n
+        self.data.push(val) #push the new value
+    
+    def delete(self):
+        ''' operator "s"'''
+        idx = self.data.peek() #can't use pop to get parameter n, because delete might want to use the 1st element
+        util.expectPosIntSmallerThan(idx, self.data.size())
+        val = self.data[idx] #get the value at stack depth n
+        if(idx != 0):
+            self.data.remove(idx)
+        self.data.pop() #pop parameter n
+    
+    def read(self):
+        # read from input stream 
+        item = chr(self.iS.read())
+        # only digits, commands and operators may be read 
+        if util.is_number(item):
+            self.data.push(int(item))
+        elif item in self.operations:
+            self.data.push(item)
+    
+    def write(self):
+        '''operation "w"'''
+        # make sure that the top element of the data stack is an integer or a command
+        val = self.data.pop()
+        if not util.is_number(val) and val not in self.operations:
+            raise ValueError("Cannot write to output stream" + val)
+        for byte in to_ascii(val):
+            self.oS.write(byte)
+    
+    def applyBlock(self):  #note that "apply" is a reserved word
+        ''' operation "a" '''
+        if self.is_block(self.data.peek()):
+            p = self.data.pop()
+            self.push_code(p[1:len(p) - 1])
+      
+    def build(self):
+        ''' operation "b" '''
+        arg = self.data.pop()
+        if self.is_block(arg) or arg in self.operations:
+            self.data.push("[" + arg + "]")
+        else:
+            raise ValueError("Invalid argument for operation 'build block':" + str(arg))
+      
+    def group(self):
+        ''' operation "g" '''
+        arg1 = self.data.pop() # the argument on top of the stack will be at the end of the block
+        arg2 = self.data.pop()
+        if self.is_block(arg1) and self.is_block(arg2):
+            # two blocks
+            block =  arg2[0:len(arg2) - 1]
+            arg1 = arg1[1:len(arg1)]
+            if util.is_number(arg2[len(arg1) - 1]) and util.is_number(arg1[0]):
+                    block += " "
+            block += arg1
+        elif util.is_number(arg1) and util.is_number(arg2):
+            # two integers
+            block = "[" + str(arg2) + " " + str(arg1) + "]"
+        elif util.is_number(arg2) and self.is_block(arg1):
+            # interger (arg2) and block (arg1)
+            block = "[" + str(arg2)
+            arg1 = arg1[1:len(arg1)]
+            if util.is_number(arg1[0]):
+                    block += " "
+            block += arg1
+        elif self.is_block(arg2) and util.is_number(arg1):
+            # block (arg2) and integer (arg1)
+            block =  arg2[0:len(arg2) - 1]
+            if util.is_number(arg2[len(arg2) - 1]) and util.is_number(arg1[0]):
+                    block += " "
+            block += str(arg1) + "]"
+        else:
+            raise ValueError("todo")
+        self.data.push(block)
+        
     def div(self, a, b):
         '''operator "/"'''
         return self.op_div(operator.div, a, b)
