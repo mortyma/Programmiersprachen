560408e794cfc08f122775e60445d3828e3f8720
Sebastian Arming <sebastian@arming.net>
2014-07-23 01:39:38 +0200

    partially working

--- assignment2/parser.rb
+++ assignment2/parser.rb
@@ -34,12 +34,27 @@ class GCParser < Parslet::Parser
   root :program
 end
 
-class Variable < Struct.new(:name)
+class Variable 
+ 
+  def initialize(name)
+    @name=name.to_str
+  end
+
+ @@finally=Variable.new('finally')
+  def self.finally
+    @@finally
+  end
+
+  def name
+    @name
+  end
+
   def expand(ctx)
     ctx.key? @name or raise "Cannot expand"
     ctx[@name]
   end
   def bound?(ctx)
+    # pp 'var' ,@name, ctx.key?( @name)
     ctx.key? @name
   end
 end
@@ -48,7 +63,7 @@ class Str < Struct.new(:contents)
   def expand(ctx)
     contents.map{|x|
       x.is_a?(Variable) ? x.expand(ctx) : x
-    }.join("")
+    }.join('')
   end
   def bound?(ctx)
     contents.all?{|x| x.is_a?(String) or x.bound?(ctx) }
@@ -57,67 +72,123 @@ end
 
 class GCommand < Struct.new(:guards, :command)
   def ready?(ctx)
-    guards.all{|g| g.ready?} and command.ready?
+    # pp command.ready?(ctx), guards.map{|g| [g,g.ready?(ctx)]}
+    guards.all?{|g| g.ready?(ctx)} and command.ready?(ctx)
   end
   def run(ctx)
-    command.run(ctx)
+    command.run(ctx) if guards.all?{|g| g.true?(ctx)} 
   end
 end
 
-class Guard < Struct.new(:data)
+class Atom
+  def initialize(lvs=[],rvs=[], &block)
+    @lvs = lvs
+    @rvs = rvs
+    @block = block
+  end
+
   def ready?(ctx)
-    @lv.all?{|s| s.bound?}
+    @rvs.all?{|x| x.nil? or x.bound?(ctx)} and @lvs.all?{|x| x.nil? or not x.bound?(ctx)}
+  end
+
+  def call(ctx)
+    params = @rvs.map{|x| x.nil? ? nil : x.expand(ctx)}
+    @block.call(*params)
+  end
+
+  def run(ctx)
+    l=@lvs.map{|l| l.nil? ?nil: l.name}
+    ret=Hash[l.zip(call(ctx))]
+    ret.delete(nil)
+    # pp "update", Hash[@lvs.zip(call(ctx))], ret
+    ctx.merge!(ret)
   end
 end
 
-class Command < Struct.new(:data)
-  def ready?(ctx)
-    @lv.all?{|v| not v.bound?(ctx)} and @rv.all?{|s| s.bound?}
+class Guard < Atom
+  def initialize(rvs, &block)
+    block ||= lambda {True}
+    super([],rvs, &block)
+  end
+
+  def true?(ctx)
+    ready?(ctx) and call(ctx)
   end
 end
 
-class Procedure < Struct.new(:data)
+class Command < Atom
+
+end
+
+class Procedure
+   def initialize(name,params,results, commands)
+    @name=name
+    @params=params
+    @results=results
+    @commands=commands
+
+   end
   def finished?(ctx)
     @results.all?{|s|s.bound?(ctx)}
   end
 
   def run(*args)
-    ctx={}
-    # while(not finished?(ctx)) do
-    #     @gcommands.each do |c|
-    #       c.run(ctx) if c.ready?(ctx)
-    #     end
-    #   end      1
+    ctx={'a'=>1,'b'=>2,'c'=>3}
+    while(not finished?(ctx)) do
+    #for i in 1..5 do
+         @commands.each do |c|
+           c.run(ctx) if c.ready?(ctx)
+         end
+       end
+    ctx   
   end
+end
+
+class ActivationRecord
+
 end
 
 
 class GCAst < Parslet::Transform
   rule(:name => simple(:x)) { Variable.new(x) }
   rule(:content => simple(:x)) { x.to_str }
-  rule(:string => subtree(:x)) { s=Str.new(x);s.bound?({}) and pp s.expand({}); s }
+  rule(:string => subtree(:x)) { s=Str.new(x) }
 
-  rule(:l => simple(:l), :eq => simple(:eq), :r =>simple(:r) ) { Guard.new([l,:eq,r]) }
-  rule(:l => simple(:l), :neq => simple(:eq), :r =>simple(:r) ) { Guard.new([l,:neq,r]) }
-  rule(:finally => simple(:x)) { Guard.new(:finally) }
-  rule(:isbound => simple(:x)) { Guard.new([:isbound,x]) }
+  rule(:l => simple(:l), :eq => simple(:eq), :r =>simple(:r) ) { Guard.new([l,r]){|l, r| l==r} }
+  rule(:l => simple(:l), :neq => simple(:eq), :r =>simple(:r) ) { Guard.new([l,r]){|l, r| l!=r} }
+  rule(:finally => simple(:x)) { Guard.new([Variable.finally]) }
+  rule(:isbound => simple(:x)) { Guard.new([x]) }
 
-  rule(:result => simple(:result), :stdout => simple(:stdout), :cmd => simple(:cmd), :stdin=>simple(:stdin)) { Command.new([:exec, result,stdout,cmd,stdin]) }
-  rule(:fst => simple(:fst), :snd => simple(:snd), :delim => simple(:delim), :str=>simple(:str)) { Command.new([:split, fst,snd,delim, str]) }
-  rule(:results => sequence(:results), :procname =>simple(:procname), :params => sequence(:params)) { Command.new([:call, results, procname, params]) }
-  rule(:l => simple(:l), :r=>sequence(:r)) { Command.new([:assignment, l,r]) }
+  rule(:result => simple(:result), :stdout => simple(:stdout), :cmd => simple(:cmd), :stdin=>simple(:stdin)) {
+    Command.new([result,stdout],[cmd,stdin]){|cmd,stdin|
+    ["1","out"]
+    }
+  }
+  rule(:fst => simple(:fst), :snd => simple(:snd), :delim => simple(:delim), :str=>simple(:str)) {
+    Command.new([fst,snd],[delim, str]){|delim,str|
+      str.split(delim,2)
+    }
+  }
+  rule(:results => sequence(:results), :procname =>simple(:procname), :params => sequence(:params)) {
+    Command.new(results,[procname, params].flatten){|procname, *params|
+    []
+    }
+  }
+  rule(:l => simple(:l), :r=>sequence(:r)) {
+    Command.new([l],r){|*r| [r.join('')] }
+  }
 
   rule(:guards => sequence(:g), :command=> simple(:c)) { GCommand.new(g,c) }
 
-  rule(:procname => simple(:n), :params => sequence(:p), :results=>sequence(:r), :gcommands => sequence(:c)) { Procedure.new([n, p,r,c]) }
+  rule(:procname => simple(:n), :params => sequence(:p), :results=>sequence(:r), :gcommands => sequence(:c)) { Procedure.new(n, p,r,c) }
   rule(:program => sequence(:p)) {p}
 
 end
-
+  
 def parse(str)
   gcp = GCParser.new
   gct = GCAst.new
-  gct.apply(gcp.parse(str))
+  procs=gct.apply(gcp.parse(str))
 rescue Parslet::ParseFailed => failure
   puts failure.cause.ascii_tree
 end
@@ -125,19 +196,20 @@ end
 require 'pp'
 
 pp parse(<<END
-         maxnum a b c -  "$max$" {
+         maxnum a b c -  "$max$$two$" {
            ab = exec "test $a$ -le $b$";
            bc = exec "test $b$ -le $c$";
-           ab  : max = maxnum;
-           ab == "1" : bc == "0" : ac  b = split "s" "test $a$ -le $c$";
-
            ab == "0" : bc == "0" : max = "$c$";
            ab == "0" : bc == "1" : max = "$b$";
            ab == "1" : bc == "1" : max = "$a$";
            ab == "1" : bc == "0" : ac = exec "test $a$ -le $c$";
            ac == "0" : max = "$c$";
            ac == "1" : max = "$a$";
+           one two = split "b" "1b2";
            finally : max = "error";
          }
 END
-)
+)[0].run()
+
+# ab  : max = maxnum;
+#            ab == "1" : bc == "0" : ac  b = split "s" "test $a$ -le $c$";
