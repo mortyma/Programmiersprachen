1fd7164ca41ec4c96b5857c8f0b9fb1922d68f48
Sebastian Arming <sebastian@arming.net>
2014-09-17 17:10:25 +0200

    ass2: interpreter uses 6 threads

--- assignment2/interpreter.rb
+++ assignment2/interpreter.rb
@@ -2,14 +2,13 @@
 require_relative 'parser'
 
 program = parse( ARGF.read )
-puts program
-puts program.run('BEGIN')
+# puts program.run('BEGIN')
 
 
-# q = program.queue_for_call('Run') { |ret| 
-#   puts ret
-#   q.kill
-# }
+q = program.queue_for_call('BEGIN') { |ret| 
+  puts ret
+  q.kill
+}
 
-# workers = 1.upto(6).map { Thread.new { q.run } }
-# workers.each{|t| t.join }
+workers = 1.upto(6).map { Thread.new { q.run } }
+workers.each{|t| t.join }
